e619a6a9bdab8f77a4cf0b32f89ae21411bde343
Charlie Jeppsson <charlie.jeppsson1@gmail.com>
2019-11-14 19:32:41 +0100

    Refactor getPosts

--- src/api/getPosts.js
+++ src/api/getPosts.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 async function getPosts({
-  type,
   currentPage,
   setCurrentPage,
   count,
@@ -12,68 +11,86 @@ async function getPosts({
   setPrevPage,
   setLoading,
   setPosts,
+  type,
 }) {
-  console.log('count before request: ', count);
+  try {
+    setLoading(true);
+    const response = await axios.get(
+      'https://www.reddit.com/r/javascript.json',
+      { params: setParams(count, prevPage, nextPage, type) }
+    );
+    const filteredPosts = filterPosts(response);
+    const newPosts = formatNewPosts(filteredPosts);
+    setPosts(newPosts);
+    updateCount({ count, setCount, newCount: newPosts.length, type });
+    updateCurrentPage({ currentPage, setCurrentPage, type });
+    updatePrevAndNext({ response, setPrevPage, setNextPage });
+    setLoading(false);
+  } catch (err) {
+    console.log(err);
+    setLoading(false);
+  }
+}
 
+function setParams(count, prevPage, nextPage, type) {
   let params = { count, limit: 10 };
   if (type === 'next') {
     params.after = nextPage;
   } else if (type === 'prev') {
     params.before = prevPage;
   }
-  console.log('params: ', params);
+  return params;
+}
 
-  try {
-    setLoading(true);
-    const response = await axios.get(
-      'https://www.reddit.com/r/javascript.json',
-      { params: { ...params } }
-    );
-    console.log('res: ', response);
+function filterPosts(response) {
+  // On the first page Reddit will ignore the limit and also pass along posts
+  // marked with stickied: true. Hence, we only want to display and count the
+  // ones with stickied: false to only show 10 posts per page.
+  return response.data.data.children.filter(post => (
+    !post.data.stickied
+  ));
+}
 
-    // Update posts:
-    // On the first page Reddit will ignore the limit and also pass along posts
-    // marked with stickied: true. Hence, we only want to display and count the
-    // ones with stickied: false to only show 10 posts per page.
-    const filteredPosts = response.data.data.children.filter(post => (
-      !post.data.stickied
-    ));
-    const newPosts = filteredPosts.map(post => (
-      {
-        id: post.data.id,
-        author: post.data.author,
-        created: post.data.created_utc,
-        commentCount: post.data.num_comments,
-        permalink: post.data.permalink,
-        score: post.data.score,
-        selftext: post.data.selftext,
-        thumbnail: post.data.thumbnail,
-        title: post.data.title,
-      }
-    ));
-    setPosts(newPosts);
-
-    // Update count and currentPage for next request
-    if (type === 'first') {
-      setCount(count + newPosts.length);
-      setCurrentPage(currentPage + 1);   
-    } else if (type === 'next') {
-      setCount(count + newPosts.length);
-      setCurrentPage(currentPage + 1);   
-    } else if (type === 'prev') {
-      setCount(count - newPosts.length);
-      setCurrentPage(currentPage - 1);   
+function formatNewPosts(filteredPosts) {
+  return filteredPosts.map(post => (
+    {
+      id: post.data.id,
+      author: post.data.author,
+      created: post.data.created_utc,
+      commentCount: post.data.num_comments,
+      permalink: post.data.permalink,
+      score: post.data.score,
+      selftext: post.data.selftext,
+      thumbnail: post.data.thumbnail,
+      title: post.data.title,
     }
+  ));
+}
 
-    // Update prevPage and nextPage for next request
-    const { before, after } = response.data.data;
-    setPrevPage(before);
-    setNextPage(after);
-    setLoading(false);
-  } catch (err) {
-    console.log(err);
-    setLoading(false);
+function updateCount(args) {
+  const { count, setCount, newCount, type } = args;
+  if (type === 'first' || type === 'next') {
+    setCount(count + newCount);
+  } else if (type === 'prev') {
+    setCount(count - newCount);
   }
+}
+
+function updateCurrentPage(args) {
+  const { currentPage, setCurrentPage, type } = args;
+  if (type === 'first' || type === 'next') {
+    setCurrentPage(currentPage + 1);   
+  } else if (type === 'prev') {
+    setCurrentPage(currentPage - 1);   
+  }
+}
+
+function updatePrevAndNext(args) {
+  // Update prevPage and nextPage for next request
+  const { response, setPrevPage, setNextPage } = args;
+  const { before, after } = response.data.data;
+  setPrevPage(before);
+  setNextPage(after);
 }
 
 export default getPosts;
